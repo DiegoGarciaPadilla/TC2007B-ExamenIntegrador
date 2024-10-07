a3b5f253e731dc12044655973cf915c568972abb
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-10-07 09:04:05 -0600

    feat: init xcode project

---
+++ ExamenIntegrador/ExamenIntegrador/ExamenIntegradorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenIntegradorApp.swift
+//  ExamenIntegrador
+//
+//  Created by Diego Antonio García Padilla on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ExamenIntegradorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
